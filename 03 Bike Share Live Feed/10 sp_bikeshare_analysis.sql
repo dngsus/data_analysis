a259b0ae311940eefdf8dfbb49b830caca6f17ac
dngsus <daniel.ng2507@gmail.com>
2025-09-04 13:07:04 +0100

    cosmetics

--- 03 Bike Share Live Feed/10 sp_bikeshare_analysis.sql
+++ 03 Bike Share Live Feed/10 sp_bikeshare_analysis.sql
@@ -23,7 +23,7 @@ BEGIN
 station_info_stg is from .py script.
 
 Insert into final from staging if:
-- Totally new station_id
+- Totally new station_id, or;
 - Existing station_id, but information e.g. capacity has changed (SCD)
 
 Retain (station_id + info) in final even if does not exist in staging (do not wipe from final table if latest pull into staging doesn't include it anymore)
@@ -34,7 +34,7 @@ insert into dbo.station_info
 station_id, name, short_name, capacity, region_id, lat, lon, valid_from, valid_to
 )
 select stg.station_id, stg.name, stg.short_name, stg.capacity,
-case when stg.region_id is not null then stg.region_id else 0 end,
+case when stg.region_id is not null then stg.region_id else 0 end, -- nulls impact aggregation etc., better to just place in a throwaway category
 stg.lat, stg.lon, stg.valid_from, null -- default valid_to is null := indefinitely true
 from station_info_stg stg
 left join station_info info
@@ -42,7 +42,7 @@ left join station_info info
 	and info.valid_to is null -- specifically comparing details of latest call vs latest saved entry.
 where
 	info.station_id is null -- totally new station_id
-	or ( -- or, any new information about an existing station_id (based on matching station_id)
+	or ( -- or, there is any new information about an existing station_id (based on matching station_id)
        info.name <> stg.name
 	   or info.short_name <> stg.short_name
        or info.capacity <> stg.capacity
@@ -81,32 +81,34 @@ select * from dbo.station_info where station_id = '08265124-1f3f-11e7-bf6b-3863b
 Assume lat/long however won't change.
 Therefore, only time the regional lat/lon needs updating is when there is a totally new station inserted into dbo.station_info.
 As of now, there is no way to tell if a new insertion is for totally new station, or just updated pre-existing one.
-Nevertheless, both types of update are so infrequent, so doesn't really matter (immaterial impact on compute). So, if ever the count of PK 'info_id' increase --> run update
+Nevertheless, both types of update are so infrequent, so doesn't really matter (immaterial impact on compute).
+So, if ever the count of PK 'info_id' increase --> run update
 */
 
 if
 	(select max(info_id) from dbo.station_info) -- as the PK, info_id automatically updates (increases) when new row inserted
-	> (select max(max_info_id) from dbo.station_info_updates)
+	> (select max(max_info_id) from dbo.station_info_updates) -- if this condition is true, we have a (potential) new insertion
 begin
 	with unique_info as -- station_info contains possible dupes of station_id (historic record)
 	-- If duplicates remained, then the final lat/lon would gravitate towards more severely duplicated stations.
 	(
-	select min(lat) lat, min(lon) lon, station_id, region_id -- again, assume lat/lon of a station won't change .. otherwise need figure a way to select specifically the latest lat/lon, not just the min
+	select min(lat) lat, min(lon) lon, station_id, region_id -- again, we assume lat/lon of a station won't change .. otherwise need figure a way to select specifically the latest lat/lon, not just the min
 	from dbo.station_info
 	group by station_id, region_id
 	),
 	two as
 	(
-	select reg.region_id, reg.name, avg(un.lat) lat, avg(un.lon) lon
+	select reg.region_id, reg.name, avg(un.lat) lat, avg(un.lon) lon -- average of lat/lon of stations within each region (group) --> representative lat/lon of the region
 	from dbo.regions reg
 		join unique_info un on reg.region_id = un.region_id
 	group by reg.region_id, reg.name
 	) 
 	update dbo.regions
-	set lat = two.lat,
-	lon = two.lon
+	set
+		lat = two.lat,
+		lon = two.lon
 	from dbo.regions reg
-	join two on reg.region_id = two.region_id
+		join two on reg.region_id = two.region_id
 
 	-- Insert new entry into tracker
 	insert into dbo.station_info_updates
@@ -128,11 +130,12 @@ delete from dbo.station_status where report_date = '1970-01-01'; -- investigate 
 /*
 (1) Populate columns
 
--- window funcs only acceptable in select, order by, and some other cases
--- need CTE as wrapper to enable update values
+(Window funcs only acceptable in select, order by, and some other cases - need CTE as wrapper to enable update values)
 */
 
--- Need this one first as stn_info_id UPDATE is dependent
+-- Correct the formating of datetime column.
+-- Need to do this first as stn_info_id UPDATE is dependent
+
 with temp as 
 (
 select
@@ -147,8 +150,9 @@ where cast(retrieved_at as date) = @today; -- once up and running, only want to 
 
 -- Populate stn_info_key to enable match between status.stn_info_id = info.info_id
 -- Since info.station_id is not unique, as there may be SCD
-
+-- I.e. station_id refers to a station, but the 'same' (underlying) station may exist in various 'information statuses' due to SCD
 -- Need this here as future UPDATE statements are dependent
+
 update stat
 set stat.stn_info_id = info.info_id
 from dbo.station_status stat
@@ -156,6 +160,7 @@ join dbo.station_info info
 	on stat.station_id = info.station_id
 	and stat.report_datetime >= info.valid_from
 	and (stat.report_datetime < info.valid_to or info.valid_to is null)
+	-- Realistically, only "info.valid_to is null" is in play, since the default value of valid_to is null
 where cast(stat.retrieved_at as date) = @today; -- once up and running, only want to use compute to update a limited set of rows
 
 -- Helper column: lag function to retrieve previous reading from station
@@ -182,21 +187,10 @@ where cast(retrieved_at as date) = @today;
 -- and station_id = '000_test' and capacity is null;
 -- if null, then this is first ever record of the station_id, so status_duration_seconds is null
 
--- Flag entries where duration too long (can't safely assume dock availability changes)
--- Can't delete, as this would break dataflow - after a single missed API call, and subsequent deletion of incoming rows,
--- new rows could never again 'survive'
-
 /*
-Delete from table where status_duration > 1800 seconds (30 mins)
--- Or is NULL --> first reading ever for that stn_id
--- Not conducive towards record-keeping, but for a project with this scope and context, will proceed
-
-delete from dbo.station_status
-where
---(
-status_duration_seconds > 1800
-or status_duration_seconds is null;
---) and station_id = '000_test' and capacity is null
+Flag but don't delete entries where duration too long (can't safely assume dock availability changes)
+Can't delete, as this would break dataflow - after a single missed data pull, and subsequent deletion of incoming rows,
+new rows could never again 'survive'
 */
 
 update dbo.station_status
@@ -209,37 +203,49 @@ where cast(retrieved_at as date) = @today;
 
 update dbo.station_status
 set status_duration_yday = 
-case when report_date = dateadd(d, 1, cast(prev_report_datetime as date)) 
-then 
-  DATEDIFF(SECOND, prev_report_datetime, report_date)
-  else NULL end
+	case
+		when report_date = dateadd(d, 1, cast(prev_report_datetime as date)) 
+			then datediff(second, prev_report_datetime, report_date)
+		else NULL
+	end
  where cast(retrieved_at as date) = @today; 
  -- where station_id = '000_test' and capacity is null
 
 update dbo.station_status
-set [status_duration_today] = case when status_duration_yday is not null then status_duration_seconds - status_duration_yday
-else status_duration_seconds end
+set [status_duration_today] =
+	case
+		when status_duration_yday is not null
+			then status_duration_seconds - status_duration_yday
+		else status_duration_seconds
+	end
 where cast(retrieved_at as date) = @today; 
  -- where station_id = '000_test' and capacity is null
 
--- Move this here because will be wiping report_time in next steps
--- Categorise readings based on day of week + time of day
--- Weekend vs weekday
--- Rush-hour := (0700 - 0900; 1700 - 1900) on weekdays only
--- Would depend on hypothetical business standards
--- If wanting even more end-user customisability over times of interest, probably need to do as measures in PBI
+ /*
+Move this here because will be wiping report_time in next steps.
+Categorise readings based on day of week + time of day:
+	- Weekend vs weekday
+	- Rush-hour := (0700 - 0900; 1700 - 1900) on weekdays only
+
+(Would depend on hypothetical business standards.
+If wanting even more end-user customisability over times of interest,
+probably need to do as measures in PBI or reconfigure SQL script)
+*/
 
 update dbo.station_status
 set [weekday] = case when datepart(dw, report_date) in (1, 7) then 0 else 1 end
 where cast(retrieved_at as date) = @today; 
 --where station_id = '000_test' and capacity is null
 
--- For this hypothetical business, rush_hour is accepted to be 7am - 9am, 5pm - 7pm
--- Noting that report_time from station appears to be typically 5 minutes before the API request,
--- and API requests are scheduled for every 1/4 hour,
--- then we can approximate a rule-time for 7.10am and 5.10pm.
--- There will be slightly errant data due to imperfect timings, but should be good enough.
--- Notice: a reading with report_time = 07:01:00 is essentially 6.45-7am, so basically not rush hour
+/*
+For this hypothetical business, rush_hour is accepted to be 7am - 9am, 5pm - 7pm
+Noting that report_time from station appears to be typically 5 minutes before the API request,
+and API requests are scheduled for every 1/4 hour,
+then we can approximate a rule-time for 7.10am and 5.10pm.
+There will be slightly errant data due to imperfect timings, but should be good enough.
+
+Notice: a reading with report_time = 07:01:00 is essentially 6.45-7am, so basically not rush hour
+*/
 
 update dbo.station_status
 set rush_hour =
@@ -256,8 +262,8 @@ where cast(retrieved_at as date) = @today;
 update stat
 set stat.capacity = info.capacity
 from dbo.station_info info
-join station_status stat
-on info.info_id = stat.stn_info_id
+	join station_status stat
+		on info.info_id = stat.stn_info_id
 where cast(retrieved_at as date) = @today; -- once up and running, only want to use compute to update a limited set of rows
 --where stat.station_id = '000_test' and stat.capacity is null
 
@@ -269,8 +275,7 @@ where cast(retrieved_at as date) = @today; -- or station_id = '000_test';
 
 -- Categorical definitions of above percentages
 
---select distinct capacity from dbo.station_info order by 1
--- 8 to 55
+--select distinct capacity from dbo.station_info order by 1 --> 8 to 55
 -- If smaller stations emerge, might have to adapt logic, as num_docks / num_bikes available <= 2 is somewhat expected, arguably not 'near full/empty'
 
 update dbo.station_status
@@ -290,7 +295,7 @@ Generate fact table which accounts for edge cases (overnights),
 whereby an overnight entry has a portion attributed to yesterday (yday), and also today.
 
 As we split edge cases into two rows, also replace original report_date column, and drop report_time (won't make sense anymore in the case of overnights),
-and prev_report_datetime has served its purpose as a helper column.
+and prev_report_datetime has already served its purpose as a helper column.
 
 --> Define new table dbo.station_status_by_day to encompass this data - technically not necessarily 'by day' if there is a > 24hr gap in recordings
 (there would be no rows with the interim date(s)).
@@ -307,14 +312,14 @@ capacity, pc_docks_available, availability_group)
 select
     stn_info_id, station_id, is_installed, num_bikes_any_available, num_bikes_pedal_available, num_bikes_elec_available, is_renting, num_docks_available, is_returning,
 	-- report_date, report_time,
-	DATEADD(DAY, -1, report_date), -- report_date
+	DATEADD(DAY, -1, report_date), -- the true report_date of the 'previous night' portion of an overnight entry is yday
 	weekday, rush_hour, retrieved_at, -- prev_report_datetime,
 	status_duration_yday, -- duration_seconds
 	capacity, pc_docks_available, availability_group
 from dbo.station_status ss
 where
-	status_duration_yday is not null
-	and duration_flag = 0 -- we can exclude these now. Only point to keep them in station_status is to ensure prev_report_datetime is accurate
+	status_duration_yday is not null -- special case where reading contains a 'previous night' portion due to overnight nature
+	and duration_flag = 0 -- we can exclude these now. Only point to keep them in station_status was to ensure prev_report_datetime is accurate
 	and not exists (select 1 from dbo.station_status_by_day byday where ss.station_id = byday.station_id and ss.retrieved_at = byday.retrieved_at)
 
 union all
@@ -330,8 +335,9 @@ from dbo.station_status ss
 where
 	duration_flag = 0 -- we can exclude these now. Only point to keep them in station_status is to ensure prev_report_datetime is accurate
 	and not exists (select 1 from dbo.station_status_by_day byday where ss.station_id = byday.station_id and ss.retrieved_at = byday.retrieved_at)
--- If an overnight entry was split Sunday/Monday, it would have read as weekday = 0, as today = monday,
--- but now with the new Sunday entry, there needs to be weekday = 1 for that --> reset weekday flag accordingly
+
+
+-- If an overnight entry was split across Sunday/Monday, the 'weekday' flag is now incorrect after splitting as per above step. Need to reset:
 
 update dbo.station_status_by_day
 set [weekday] = case when datepart(dw, report_date) in (1, 7) then 0 else 1 end
